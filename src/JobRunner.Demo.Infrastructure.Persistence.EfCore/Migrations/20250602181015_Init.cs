bb9241924636fcdac539c6d4fa583342a1bb2eb6
e-matveev <e-matveev@asoft21.ru>
2025-06-02 21:16:24 +0300

    feat: add init migration

---
+++ src/JobRunner.Demo.Infrastructure.Persistence.EfCore/Migrations/20250602181015_Init.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobRunner.Demo.Infrastructure.Persistence.EfCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "jobs");
+
+            migrationBuilder.CreateTable(
+                name: "cs_task_schedules",
+                schema: "jobs",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()", comment: "Идентификатор"),
+                    c_name = table.Column<string>(type: "text", nullable: false, comment: "Название"),
+                    c_description = table.Column<string>(type: "text", nullable: true, comment: "Описание"),
+                    c_cron_expression = table.Column<string>(type: "text", nullable: false, comment: "Cron-выражение"),
+                    b_is_enabled = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false, comment: "Признак активности"),
+                    n_max_items_per_iteration = table.Column<int>(type: "integer", nullable: false, defaultValue: 50, comment: "Количество задач из очереди, обрабатываемых за один проход"),
+                    n_concurrency_limit = table.Column<int>(type: "integer", nullable: false, defaultValue: 20, comment: "Количество параллельных задач, обрабатываемых за один проход"),
+                    j_custom_params = table.Column<string>(type: "jsonb", nullable: true, comment: "Дополнительные параметры в формате JSON"),
+                    n_max_retries = table.Column<int>(type: "integer", nullable: false, defaultValue: 3, comment: "Количество попыток повторного запуска задачи"),
+                    s_creator = table.Column<string>(type: "text", nullable: true, comment: "Создатель"),
+                    s_create_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата создания"),
+                    s_owner = table.Column<string>(type: "text", nullable: true, comment: "Владелец"),
+                    s_modif_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата изменения")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cs_task_schedules", x => x.id);
+                    table.CheckConstraint("chk_cs_task_schedules_c_name_valid", "c_name ~ '^[a-zA-Z0-9_-]+$'");
+                },
+                comment: "Конфигурация задач");
+
+            migrationBuilder.CreateTable(
+                name: "cs_task_statuses",
+                schema: "jobs",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()", comment: "Идентификатор"),
+                    c_name = table.Column<string>(type: "text", nullable: false, comment: "Название"),
+                    c_code = table.Column<string>(type: "text", nullable: false, comment: "Код"),
+                    s_creator = table.Column<string>(type: "text", nullable: true, comment: "Создатель"),
+                    s_create_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата создания"),
+                    s_owner = table.Column<string>(type: "text", nullable: true, comment: "Владелец"),
+                    s_modif_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата изменения")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cs_task_statuses", x => x.id);
+                    table.CheckConstraint("chk_cs_task_statuses_c_code_valid", "c_code ~ '^[a-zA-Z0-9_-]+$'");
+                },
+                comment: "Статусы задач");
+
+            migrationBuilder.CreateTable(
+                name: "cd_task_queue",
+                schema: "jobs",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()", comment: "Идентификатор"),
+                    j_payload = table.Column<string>(type: "jsonb", nullable: false, comment: "Параметры запуска в формате JSON"),
+                    j_error = table.Column<string>(type: "jsonb", nullable: true, comment: "Информация об ошибке в формате JSON"),
+                    d_start_by_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Запуск по времени (дата и время запуска)"),
+                    d_start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата запуска"),
+                    d_end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата завершения"),
+                    n_retry_count = table.Column<int>(type: "integer", nullable: false, defaultValue: 0, comment: "Количество попыток перезапуска задачи"),
+                    b_is_manual = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false, comment: "Признак ручного запуска"),
+                    f_schedule = table.Column<Guid>(type: "uuid", nullable: false, comment: "Идентификатор конфигурации"),
+                    f_status = table.Column<Guid>(type: "uuid", nullable: false, comment: "Идентификатор статуса"),
+                    s_creator = table.Column<string>(type: "text", nullable: true, comment: "Создатель"),
+                    s_create_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата создания"),
+                    s_owner = table.Column<string>(type: "text", nullable: true, comment: "Владелец"),
+                    s_modif_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, comment: "Дата изменения")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cd_task_queue", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_cd_task_queue_cs_task_schedules_f_schedule",
+                        column: x => x.f_schedule,
+                        principalSchema: "jobs",
+                        principalTable: "cs_task_schedules",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_cd_task_queue_cs_task_statuses_f_status",
+                        column: x => x.f_status,
+                        principalSchema: "jobs",
+                        principalTable: "cs_task_statuses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "Очередь задач");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cd_task_queue_f_schedule",
+                schema: "jobs",
+                table: "cd_task_queue",
+                column: "f_schedule");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cd_task_queue_f_status",
+                schema: "jobs",
+                table: "cd_task_queue",
+                column: "f_status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cs_task_schedules_c_name",
+                schema: "jobs",
+                table: "cs_task_schedules",
+                column: "c_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cs_task_statuses_c_code",
+                schema: "jobs",
+                table: "cs_task_statuses",
+                column: "c_code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cd_task_queue",
+                schema: "jobs");
+
+            migrationBuilder.DropTable(
+                name: "cs_task_schedules",
+                schema: "jobs");
+
+            migrationBuilder.DropTable(
+                name: "cs_task_statuses",
+                schema: "jobs");
+        }
+    }
+}
